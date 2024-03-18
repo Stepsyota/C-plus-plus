bcb44a038eee2b501c66f3cf9ec2dccafd52dcd9
Stepan <stepsyota@gmail.com>
2024-03-18 21:41:41 +0300

    Task was complete

--- 2 semestr/Part B Variant 7/Chapter 3/Task 3.1/Task 3.1.cpp
+++ 2 semestr/Part B Variant 7/Chapter 3/Task 3.1/Task 3.1.cpp
@@ -7,83 +7,71 @@
 
 using namespace std;
 
-void add_element(ostream&);
-void fill_by_elements(ostream&, int);
-void read_file(ifstream&);
-void add_with_sort(ifstream&, ostream&);
+void add_element(const char*, int);
+void fill_by_elements(const char*, int);
+void read_file(const char*);
+void add_with_sort(const char*, const char*, int);
+void form_files_odd_and_even(const char*, const char*, const char*);
 
 int main()
 {
 	int size = 0;
 	do {
-		cout << "Enter the even size:\n";
+		cout << "Enter the even size: ";
 		cin >> size;
 	} while (size < 1 || size % 2 != 0);
 
-	ofstream ofile_f("file_f.txt");
+	const char* file_f = "file_f.txt\0";
+	const char* file_g = "file_g.txt\0";
 
-	if (!ofile_f)
-	{
-		cout << "Not open" << endl;
-		return 1;
-	}
-	fill_by_elements(ofile_f, size);
-	ofile_f.close();
-
-
-	ifstream ifile_f("file_f.txt");
-	if (!ifile_f)
-	{
-		cout << "error2" << endl;
-		return 1;
-	}
-	read_file(ifile_f);
-	ifile_f.close();
-
-	ifile_f.open("file_f.txt");
-	if (!ifile_f)
-	{
-		cout << "Not open" << endl;
-		return 1;
-	}
-	ofstream ofile_g("file_g.txt");
-	if (!ofile_g)
-	{
-		cout << "Not open" << endl;
-		return 2;
-	}
-	add_with_sort(ifile_f, ofile_g);
-	ifile_f.close();
-	ofile_g.close();
-
-	ifstream ifile_g("file_g.txt");
-	if (!ifile_g)
-	{
-		cout << "Not open" << endl;
-		return 3;
-	}
-	read_file(ifile_g);
-	ifile_g.close();
+	fill_by_elements(file_f, size);
+	read_file(file_f);
+	
+	add_with_sort(file_f, file_g, size);
+	read_file(file_g);
 }
 
 
-void read_file(ifstream& infile)
-{
+void read_file(const char* file_name)
+ {
+	ifstream file;
+	file.open(file_name);
+	if (!file)
+	{
+		cout << "Can`t open" << endl;
+		exit(1);
+	}
 	int element;
-	infile >> element;                            
-	while (!infile.fail())
+	file >> element;                            
+	while (!file.fail())
 	{
 		cout << element << endl;
-		infile >> element; 
+		file >> element; 
 	}
 	cout << endl;
+	file.close();
 }
-void add_element(ostream& file, int element)
+void add_element(const char* file_name, int element)
 {
-	file << element << '\n';;
+	ofstream file;
+	file.open(file_name);
+	if (!file)
+	{
+		cout << "Can`t open" << endl;
+		exit(2);
+	}
+	file << element << '\n';
+	file.close();
 }
-void fill_by_elements(ostream& file, int size)
+void fill_by_elements(const char* file_name, int size)
 {
+	ofstream file;
+	file.open(file_name);
+	if (!file)
+	{
+		cout << "Can`t open" << endl;
+		exit(3);
+	}
 	int counter_even_now = 1, counter_odd_now = 1;
 	int counter_even = size / 2, counter_odd = size / 2;
 	for (int i = 0; i < size; ++i)
@@ -109,15 +97,73 @@ void fill_by_elements(ostream& file, int size)
 		{
 			counter_odd_now++;
 		}
-		add_element(file, element);
+		file << element << '\n';
 	}
+	file.close();
 }
-void add_with_sort(ifstream& file_f, ostream& file_g)
+void form_files_odd_and_even(const char* file_main_name, const char* file_odd_name, const char* file_even_name)
 {
-	while (!file_f.fail())
+	ifstream file_main;
+	ofstream file_odd;
+	ofstream file_even;
+	file_main.open(file_main_name);
+	file_odd.open(file_odd_name);
+	file_even.open(file_even_name);
+	int element;
+	file_main >> element;
+	while (!file_main.fail())
 	{
-		int element;
-		file_f >> element;
-		add_element(file_g, element);
+		if (element % 2 == 1)
+		{
+			file_odd << element << '\n';
+		}
+		else
+		{
+			file_even << element << '\n';
+		}
+		file_main >> element;
 	}
+	file_main.close();
+	file_odd.close();
+	file_even.close();
+}
+void add_with_sort(const char* file_name_f, const char* file_name_g, int size)
+{
+	ofstream file2;
+	file2.open(file_name_g);
+	if (!file2)
+	{
+		cout << "Can`t open" << endl;
+		exit(5);
+	}
+
+	const char* file_odd_name = "file_odd.txt";
+	const char* file_even_name = "file_even.txt";
+	
+	form_files_odd_and_even(file_name_f, file_odd_name, file_even_name);
+	ifstream file_odd;
+	ifstream file_even;
+
+	file_odd.open(file_odd_name);
+	file_even.open(file_even_name);
+	cout << "\nOdd\n";  read_file(file_odd_name);
+	cout << "\nEven\n";  read_file(file_even_name);
+	int element;
+	for (int i = 0; i < size / 4; ++i)
+	{
+		for (int j = 0; j < 2; ++j)
+		{
+			file_even >> element;
+			file2 << element << '\n';
+		}
+		for (int j = 0; j < 2; ++j)
+		{
+			file_odd >> element;
+			file2 << element << '\n';
+		}
+	}
+
+	file_odd.close();
+	file_even.close();
+	file2.close();
 }
